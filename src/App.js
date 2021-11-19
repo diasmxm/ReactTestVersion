621a8730bcc95d972ab30cca1bc220ec59d9dae7
diasmxm <stronghold996@mail.ru>
2021-11-19 20:12:04 +0300

    disa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './HelloWorld';
+import Post from './post';
+import posts from './posts.json';
 
 function App() {
+  const post_list = posts.map(post=>{
+    return <Post key={post.id} title={post.title} body={post.body}/>
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {post_list}
+      <HelloWorld text='Привет' color='red'/>
     </div>
   );
 }
