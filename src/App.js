eafbd67334defc4251ef83f49e37615d4544dc97
diasmxm <stronghold996@mail.ru>
2021-11-26 22:07:04 +0300

    work with sass and card and gallery

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import './App.css';
-import HelloWorld from './HelloWorld';
-import Post from './post';
-import posts from './posts.json';
+import Post from './Posts/Posts';
+import Gallery from './Gallery';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/blog">blog</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
   const post_list = posts.map(post=>{
     return <Post key={post.id} title={post.title} body={post.body}/>
   })
